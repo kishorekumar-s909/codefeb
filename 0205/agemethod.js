481bea2f1ebb97f13841d9796aa52f68b8a650c7
kishorekumar-s909 <kishoreram.mdu@gmail.com>
2026-02-11 22:36:30 +0530

    today

--- 0205/agemethod.js
+++ 0205/agemethod.js
@@ -1,18 +1,22 @@
-function findage(x,y){
+function findage(x=2000,y=2026){
+    // callback()
     let z =y-x;
     return z;
-}
-findage(2000,2026)
+   
+    }
+let first=findage()
 
- function findage1(){
-   // let g=findage();
-    if(findage()>18){
+function findage1(){
+      if(first > 18){
         console.log( " eligible for voting");
         
     }
     else{
         console.log(" not eligible for voting");
-        
     }
- }
- findage1()
+}
+        
+    
+
+
+findage1()
